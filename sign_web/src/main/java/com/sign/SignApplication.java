bbea5d9748398c7d98ed5c850097636692dbd445
gcm <gcm@126.com>
2020-04-22 15:09:49 +0800

    init

---
+++ sign_web/src/main/java/com/sign/SignApplication.java
@@ -0,0 +1,29 @@
+package com.sign;
+
+import com.sign.config.SpringContextHolder;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author MM
+ * @description
+ * @create 2020-04-22 14:36
+ **/
+@MapperScan("com.sign")
+@SpringBootApplication
+public class SignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignApplication.class, args);
+    }
+
+    @Bean
+    public SpringContextHolder springContextHolder(ApplicationContext applicationContext) {
+        SpringContextHolder springContextHolder = new SpringContextHolder();
+        springContextHolder.setApplicationContext(applicationContext);
+        return springContextHolder;
+    }
+}
